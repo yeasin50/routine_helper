5c07d58fd79bae0f0d38044f3efb57253ce1426f
yeasin50 <yeasinsheikh50@gmail.com>
2022-05-14 12:20:48 +0600

    generated-selectedRegisterCourse-chip

--- lib/routine/view/class_selection_bar.dart
+++ lib/routine/view/class_selection_bar.dart
@@ -27,6 +27,12 @@ class _ClassSelectionViewState extends State<ClassSelectionView> {
     });
   }
 
+  /// remove selected on chip-cross tap
+  void _removeRegisteredCourse(RegisteredCourse course) {
+    _selectedCourse.remove(course);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return GlassMorphism(
@@ -40,43 +46,48 @@ class _ClassSelectionViewState extends State<ClassSelectionView> {
           spacing: 10,
           crossAxisAlignment: WrapCrossAlignment.center,
           children: [
-          
-            ..._selectedCourse.isEmpty
-                ? [  //show add button while non is selected 
-                    ChoiceChip(
-                      selected: true,
-                      selectedColor: Colors.deepPurple,
-                      avatar: const Material(
-                          color: Colors.blue,
-                          shape: CircleBorder(),
-                          child: Icon(Icons.add, color: Colors.white)),
-                      label: const Text(
-                        "Select Course",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onSelected: (v) => _searchCourse(),
-                    ),
-                  ]
-                : [
-                    //todo: create chip
-                    Chip(
-                      backgroundColor: Colors.cyanAccent,
-                      deleteIcon: const Icon(Icons.close, color: Colors.red),
-                      onDeleted: () {},
-                      label: Text("CSE 498"),
-                    ),
-                    //  select your regCourses
-                    ElevatedButton(
-                      onPressed: _searchCourse,
-                      child: const Icon(Icons.add),
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                      ),
-                    ),
-                  ]
+            //show add button while non is selected
+            if (_selectedCourse.isEmpty)
+              ChoiceChip(
+                selected: true,
+                selectedColor: Colors.deepPurple,
+                avatar: const Material(
+                    color: Colors.blue,
+                    shape: CircleBorder(),
+                    child: Icon(Icons.add, color: Colors.white)),
+                label: const Text(
+                  "Select Course",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onSelected: (v) => _searchCourse(),
+              )
+            else ...[
+              //show selected course
+              ..._buildSelectedRegCourseChip(),
+              //  append `_selectedCourse` list
+              ElevatedButton(
+                onPressed: _searchCourse,
+                child: const Icon(Icons.add),
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                ),
+              ),
+            ]
           ],
         ),
       ),
     );
   }
+
+  ///chip generated from `_selectedCourse` List
+  List<Chip> _buildSelectedRegCourseChip() => _selectedCourse
+      .map(
+        (course) => Chip(
+          backgroundColor: Colors.cyanAccent,
+          deleteIcon: const Icon(Icons.close, color: Colors.red),
+          onDeleted: () => _removeRegisteredCourse(course),
+          label: Text("${course.course.courseCode} ${course.section.name}"),
+        ),
+      )
+      .toList();
 }
