373eb23e4727005b36ceca5d4f4459ead2e1c2a2
yeasin50 <yeasinsheikh50@gmail.com>
2022-05-23 21:22:08 +0600

    added-section-switcher

--- lib/features/routine/view/routine_page.dart
+++ lib/features/routine/view/routine_page.dart
@@ -9,17 +9,74 @@ class RoutinePage extends StatefulWidget {
   State<RoutinePage> createState() => _RoutinePageState();
 }
 
-class _RoutinePageState extends State<RoutinePage> {
+class _RoutinePageState extends State<RoutinePage>
+    with SingleTickerProviderStateMixin {
   SemesterSchedule semesterSchedule = SemesterSchedule(
       regCourses: [], routineVersion: "x.x", semesterName: "Test");
+
+  late AnimationController controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 200),
+  );
+  late Animation<double> animation =
+      Tween<double>(begin: 0, end: 1).animate(controller);
+
+  bool useSearchDialog = false;
+
+  void _onSearchTap() {
+    setState(() {
+      useSearchDialog = !useSearchDialog;
+    });
+
+    if (useSearchDialog)
+      controller.forward();
+    else
+      controller.reverse();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        shadowColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.withOpacity(.4),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(kToolbarHeight),
+                bottomLeft: Radius.circular(kToolbarHeight),
+              ),
+            ),
+            child: InkWell(
+              onTap: _onSearchTap,
+              splashColor: Colors.amber,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(kToolbarHeight),
+                bottomLeft: Radius.circular(kToolbarHeight),
+              ),
+              child: Container(
+                width: kToolbarHeight * 1.2,
+                alignment: Alignment.center,
+                child: AnimatedIcon(
+                  icon: AnimatedIcons.search_ellipsis,
+                  color: Colors.purple,
+                  progress: animation,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            const SizedBox(height: 12),
             CourseSelectionView(
+              useSearchDialog: useSearchDialog,
               onCourseSelectionChange: (selectedCourse) {
                 semesterSchedule =
                     semesterSchedule.copyWith(classes: selectedCourse);
